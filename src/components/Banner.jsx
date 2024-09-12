1e27dbf2ac952aeccbbd27af94ebe37f5fae6f68
Juan Felipe Andrade <juanfeav36@gmail.com>
2024-09-12 01:40:30 -0500

    feat: added new images and updated components for skills section
    - Added new gradient images (color-sharp-dark-green.png, dark-green-gradient.png)
    - Modified SVG assets (meter1.svg, meter2.svg, meter3.svg)
    - Updated Banner.jsx and Skills.jsx components

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img-v3.png";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
-import TrackVisibility from 'react-on-screen';
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
@@ -52,23 +51,20 @@ export const Banner = () => {
       <Container>
         <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+
+
+              <div>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Felipe Andrade`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Software Engineer", "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
-              </div>}
-            </TrackVisibility>
+                  <p>As a full-stack software engineer, I specialize in creating seamless digital solutions. With expertise in both backend and frontend development, I turn ideas into reality through efficient, clean code that enhances user experience and performance. Let’s connect and bring your vision to life!</p>
+                  <button onClick={() => window.location.href = '#connect'}>Let’s Connect <ArrowRightCircle size={25} /></button>
+              </div>
+
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                <div>
                   <img src={headerImg} alt="Header Img"/>
-                </div>}
-            </TrackVisibility>
+                </div>
           </Col>
         </Row>
       </Container>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
 import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/color-sharp.png"
+import colorSharp from "../assets/img/color-sharp-dark-green.png"
 
 export const Skills = () => {
   const responsive = {
@@ -35,7 +35,7 @@ export const Skills = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <p>My experience covers a range of technologies, allowing me to build robust, scalable applications from front to back.<br></br>Here’s a breakdown of my top skills...</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
